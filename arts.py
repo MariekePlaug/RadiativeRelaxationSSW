c2295e9b3c2c108639819e5b914d683cc0655267
Marieke Plaug <marieke.plaug@studium.uni-hamburg.de>
2026-01-22 11:50:36 +0100

    background atmosphere co2 doubling

--- arts.py
+++ arts.py
@@ -4,7 +4,6 @@ import xarray as xr
 import pyarts3 as pyarts
 import netCDF4 as netcdf4
 
-years = [2004,2006,2007,2008,2009,2010,2013]
 
 def atm(year, timestep):
     ds = xr.open_dataset(f"data/{year}_data.nc", engine="netcdf4")
@@ -26,3 +25,21 @@ atm1 = atm(2010, 0)
 print(atm1)
 
 
+def atm_co2doubling(year, timestep):
+    ds = xr.open_dataset(f"data/{year}_data.nc", engine="netcdf4")
+
+    atm_co2doubling = xr.Dataset(
+        data_vars={
+            "co2": (("pressure"), ds.co2[timestep,:].data * 2),
+            "no2": (("pressure"), ds.no2[timestep,:].data),
+            "no": (("pressure"), ds.no[timestep,:].data),
+            "o3": (("pressure"), ds.o3[timestep,:].data),
+            "t" : (("pressure"), ds.t[timestep,:].data),
+            "u": (("pressure"), ds.u[timestep,:].data),
+            "rh" : (("pressure"), ds.rh[timestep,:].data),},
+        coords={"pressure" : ds.pressure.data}
+    )
+    return atm_co2doubling
+
+atm_co2 = atm_co2doubling(2010, 0)
+print(atm_co2)
